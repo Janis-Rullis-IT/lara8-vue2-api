a5bcfad0b5d7d2c9e29e4faabe5c1b5b10ba9ea7
Janis Rullis <janis.rullis.it@gmail.com>
2021-08-27 17:25:16 +0300

    #16 [WIP] app.js: Fix the router error

--- vue2/src/app.js
+++ vue2/src/app.js
@@ -1,16 +1,12 @@
-// import Vue from 'vue';
-// import router from "./router";
-// import App from './App.vue';
-// // import VueResource from "vue-resource";
-// // Vue.use(VueResource);
-
 // Vue.http.options.root = 'http://api.ruu.local';
-// new Vue({router, render: h => h(App)}).$mount("#app");
 
 // #16 https://www.vuemastery.com/blog/vue-3-migration-build/
+// #16 https://v3.vuejs.org/guide/migration/global-api.html#a-new-global-api-createapp
 import { createApp } from 'vue';
 import router from "./router";
 import App from './App.vue'
 
-createApp(App).mount('#app')
-// createApp({router, render: h => h(App)}).mount('#app')
+// #16 https://github.com/vuejs/vue-router-next/issues/203#issuecomment-661073394
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
